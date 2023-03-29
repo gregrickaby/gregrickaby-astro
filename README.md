78510742c414cad21807c164f4732d2223079a7e
Greg Rickaby <gregrickaby@gmail.com>
2023-03-29 09:34:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My homepage
+
+This is my homepage built with [Astro](https://astro.build).
+
+---
---
+++ src/lib/config.ts
@@ -0,0 +1,71 @@
+const config = {
+    siteTitle: 'Greg Rickaby - Building websites since 1997',
+    siteDescription: 'Building websites since 1997',
+    siteUrl: 'https://gregrickaby.com',
+    metaDescription:
+        "Greg's expertise in building websites spans over two decades, and he channels his passion for technology, literature, and photography in everything he does.",
+    name: 'Greg Rickaby',
+    jobTitle: 'Technical/Team Lead',
+    bio: '#1 Dad. Technical/Team Lead. Published Author. Photographer.',
+    email: 'greg@gregrickaby.com',
+    ogImage: '/rickaby-family-fall-2019_1175.webp',
+    socialLinks: [
+        {
+            name: 'Email',
+            icon: '/icons/email.svg',
+            url: 'mailto:greg@gregrickaby.com',
+        },
+        {
+            name: 'Twitter',
+            icon: '/icons/brand-twitter.svg',
+            url: 'https://twitter.com/gregrickaby',
+        },
+        {
+            name: 'GitHub',
+            icon: '/icons/brand-github.svg',
+            url: 'https://github.com/gregrickaby',
+        },
+        {
+            name: 'LinkedIn',
+            icon: '/icons/brand-linkedin.svg',
+            url: 'https://www.linkedin.com/in/gregrickaby',
+        },
+        {
+            name: 'Instagram',
+            icon: '/icons/brand-instagram.svg',
+            url: 'https://www.instagram.com/gregoryrickaby',
+        },
+        {
+            name: 'YouTube',
+            icon: '/icons/brand-youtube.svg',
+            url: 'https://www.youtube.com/@GregRickaby',
+        },
+        {
+            name: 'Codepen.io',
+            icon: '/icons/brand-codepen.svg',
+            url: 'https://codepen.io/gregrickaby',
+        },
+        {
+            name: 'Flickr',
+            icon: '/icons/brand-flickr.svg',
+            url: 'https://flickr.com/photos/gregrickaby',
+        },
+        {
+            name: 'WordPress',
+            icon: '/icons/brand-wordpress.svg',
+            url: 'https://profiles.wordpress.org/gregrickaby',
+        },
+        {
+            name: 'Goodreads',
+            icon: '/icons/brand-goodreads.svg',
+            url: 'https://www.goodreads.com/author/show/16999736.Greg_Rickaby',
+        },
+        {
+            name: 'Amazon Author',
+            icon: '/icons/brand-amazon.svg',
+            url: 'https://www.amazon.com/author/gregrickaby',
+        },
+    ],
+}
+
+export default config
