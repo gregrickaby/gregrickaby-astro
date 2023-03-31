91408487702d9a085ed2f3473c6bd2aba05b2e2c
Greg Rickaby <gregrickaby@gmail.com>
2023-03-31 10:49:47 -0500

    refactor

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -8,62 +8,61 @@ const config = {
     jobTitle: 'Technical/Team Lead',
     bio: '#1 Dad. Technical/Team Lead. Published Author. Photographer.',
     email: 'greg@gregrickaby.com',
-    ogImage: '/rickaby-family-fall-2019_1175.webp',
     socialLinks: [
         {
-            name: 'Email',
-            icon: '/icons/email.svg',
-            url: 'mailto:greg@gregrickaby.com',
+            name: 'Amazon Author',
+            icon: 'brand-amazon',
+            url: 'https://www.amazon.com/author/gregrickaby',
         },
         {
-            name: 'Twitter',
-            icon: '/icons/brand-twitter.svg',
-            url: 'https://twitter.com/gregrickaby',
-        },
-        {
-            name: 'GitHub',
-            icon: '/icons/brand-github.svg',
-            url: 'https://github.com/gregrickaby',
-        },
-        {
-            name: 'LinkedIn',
-            icon: '/icons/brand-linkedin.svg',
-            url: 'https://www.linkedin.com/in/gregrickaby',
-        },
-        {
-            name: 'Instagram',
-            icon: '/icons/brand-instagram.svg',
-            url: 'https://www.instagram.com/gregoryrickaby',
-        },
-        {
-            name: 'YouTube',
-            icon: '/icons/brand-youtube.svg',
-            url: 'https://www.youtube.com/@GregRickaby',
-        },
-        {
-            name: 'Codepen.io',
-            icon: '/icons/brand-codepen.svg',
+            name: 'CodePen',
+            icon: 'brand-codepen',
             url: 'https://codepen.io/gregrickaby',
         },
         {
+            name: 'Email',
+            icon: 'email',
+            url: 'mailto:greg@gregrickaby.com',
+        },
+        {
             name: 'Flickr',
-            icon: '/icons/brand-flickr.svg',
+            icon: 'brand-flickr',
             url: 'https://flickr.com/photos/gregrickaby',
         },
         {
-            name: 'WordPress',
-            icon: '/icons/brand-wordpress.svg',
-            url: 'https://profiles.wordpress.org/gregrickaby',
-        },
-        {
             name: 'Goodreads',
-            icon: '/icons/brand-goodreads.svg',
+            icon: 'brand-goodreads',
             url: 'https://www.goodreads.com/author/show/16999736.Greg_Rickaby',
         },
         {
-            name: 'Amazon Author',
-            icon: '/icons/brand-amazon.svg',
-            url: 'https://www.amazon.com/author/gregrickaby',
+            name: 'GitHub',
+            icon: 'brand-github',
+            url: 'https://github.com/gregrickaby',
+        },
+        {
+            name: 'Instagram',
+            icon: 'brand-instagram',
+            url: 'https://www.instagram.com/gregoryrickaby',
+        },
+        {
+            name: 'LinkedIn',
+            icon: 'brand-linkedin',
+            url: 'https://www.linkedin.com/in/gregrickaby',
+        },
+        {
+            name: 'Twitter',
+            icon: 'brand-twitter',
+            url: 'https://twitter.com/gregrickaby',
+        },
+        {
+            name: 'WordPress',
+            icon: 'brand-wordpress',
+            url: 'https://profiles.wordpress.org/gregrickaby',
+        },
+        {
+            name: 'YouTube',
+            icon: 'brand-youtube',
+            url: 'https://www.youtube.com/@GregRickaby',
         },
     ],
 }
